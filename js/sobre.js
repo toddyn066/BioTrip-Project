62e01b50f643dfa782e739cf35b97bb1236258a3
João Pedro <marcondesrosajp@gmail.com>
2025-06-20 10:53:27 -0300

    Update no Sobre

---
+++ js/sobre.js
@@ -0,0 +1,25 @@
+let sobre = document.querySelector('#sobre');
+
+function dinamicSobre() {
+    sobre.innerHTML = ''
+    for (let i = 0; i < noticias.length; i++) {
+        if (i == 0) {
+            sobre.innerHTML += `
+                <div class="col-12 mb-3">
+                    <h2>${noticias[i].titulo}</h2>
+                    <p>${noticias[i].resumo}</p>
+                </div>
+            `;
+        } else {
+            sobre.innerHTML += `
+                <div class="col-12 col-md-6 mb-3">
+                    <h3>${noticias[i].titulo}</h3>
+                    <p>${noticias[i].resumo}</p>
+                </div>
+            `;
+        }
+
+    }
+}
+
+dinamicSobre();
