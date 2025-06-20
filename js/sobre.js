1b0403a0fd89f7462548494e92eef7253e9e233a
LeticiaRolando <listradajaguatirica@gmail.com>
2025-06-20 19:00:28 -0300

    resolvi o bagulho da fonte das páginas sobre e termos

--- js/sobre.js
+++ js/sobre.js
@@ -6,15 +6,15 @@ function dinamicSobre() {
         if (i == 0 || i == noticias.length - 1) {
             sobre.innerHTML += `
                 <div class="col-12 mb-3">
-                    <h2>${noticias[i].titulo}</h2>
-                    <p>${noticias[i].resumo}</p>
+                    <h2 class="fonteprincipal cor1">${noticias[i].titulo}</h2>
+                    <p class="fontesecundaria">${noticias[i].resumo}</p>
                 </div>
             `;
         } else {
             sobre.innerHTML += `
                 <div class="col-12 col-md-6 mb-3">
-                    <h3>${noticias[i].titulo}</h3>
-                    <p>${noticias[i].resumo}</p>
+                    <h3 class="fonteprincipal cor1">${noticias[i].titulo}</h3>
+                    <p class="fontesecundaria">${noticias[i].resumo}</p>
                 </div>
             `;
         }
