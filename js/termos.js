9497e40d45cade1e38e336d30ca22ec391df1d9e
LeticiaRolando <listradajaguatirica@gmail.com>
2025-06-20 19:06:36 -0300

    tirei

--- js/termos.js
+++ js/termos.js
@@ -4,10 +4,10 @@ function dinamicTermos() {
     termos.innerHTML = ''
     for (let i = 0; i < 8; i++) {
         termos.innerHTML += `
-                <h2 class="tw-semibold mt-5 fonteprincipal cor1">${i + 1}. ${artigos[i].titulo}</h2>
-                <p class"fontesecundaria">${artigos[i].paragrafo01}</p>
-                <p class"fontesecundaria">${artigos[i].paragrafo02}</p>
-                <p class"fontesecundaria">${artigos[i].paragrafo03}</p>
+                <h2 class="tw-semibold mt-5 cor1">${i + 1}. ${artigos[i].titulo}</h2>
+                <p>${artigos[i].paragrafo01}</p>
+                <p>${artigos[i].paragrafo02}</p>
+                <p>${artigos[i].paragrafo03}</p>
         `;
     }
 
